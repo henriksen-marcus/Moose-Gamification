d8c78640e07b30dcdaa148ea6f1b01f6c8305347
Stål <48680589+Staal22@users.noreply.github.com>
2023-01-27 22:13:39 +0100

    Squashed commit of the following:
    commit bcda316d209b092fb087eb3a8e339bce0331376f
    Merge: 0474300 130f316
    Author: Aleksthor <89393939+Aleksthor@users.noreply.github.com>
    Date:   Fri Jan 27 20:52:08 2023 +0100
    
        Merge pull request #3 from henriksen-marcus/elg
    
        Elg & BehaviorTree
    
    commit 130f316557310d9f4fdcb19c217be3d9703c97b8
    Merge: c50f375 0474300
    Author: Aleksthor <athorset98@gmail.com>
    Date:   Fri Jan 27 20:49:14 2023 +0100
    
        Merge branch 'main' into elg
    
    commit c50f3754fc82d11f1449c382f23597fc3abf1ab8
    Author: Aleksthor <athorset98@gmail.com>
    Date:   Fri Jan 27 20:42:23 2023 +0100
    
        BehaviorTree
    
        Behavior Tree's kan nå lages.
    
        Laget et simpelt BT til elgen. den bare går rundt random.
        Testet 1000 elger og det kjørte bra.
    
        Texture til Kirkedalen også - snipping tool over goodle maps xD
    
    commit 04743008c416a1060f5008679a93e52cc82408e4
    Merge: b599ebb f4055f9
    Author: Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
    Date:   Fri Jan 27 16:57:45 2023 +0100
    
        Merge pull request #1 from henriksen-marcus/Camera
    
        Camera Controller - v1
    
    commit f9b6937025c14650549280debd5cf42cadba0cea
    Author: Aleksthor <athorset98@gmail.com>
    Date:   Fri Jan 27 16:32:16 2023 +0100
    
        Upload temp mesh
    
    commit f4055f9c1f2c051f1908b2f5e420335be7f77ff0
    Author: Aleksthor <athorset98@gmail.com>
    Date:   Fri Jan 27 16:03:01 2023 +0100
    
        Camera Controller - v1

---
+++ UNITY/MooseOrLose/Assets/Scripts/BehaviorTree/AI's/ElgBT.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using BehaviorTrees;
+using UnityEngine.AI;
+
+public class ElgBT : BehaviorTrees.BehaviorTree
+{
+    protected override Node SetupTree()
+    {
+
+        Node root = new Selector(new List<Node>
+        {
+            // List of Nodes
+            new WalkRandom(GetComponent<NavMeshAgent>(), transform)
+        });
+
+        return root;
+    }
+}
