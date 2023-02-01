35b8e48fd6ec5c43d00798b363130ebb34d42178
Stål <48680589+Staal22@users.noreply.github.com>
2023-02-01 22:11:29 +0100

    unfinished rulemanager

--- UNITY/MooseOrLose/Assets/Scripts/Managers/RuleManager.cs
+++ UNITY/MooseOrLose/Assets/Scripts/Managers/RuleManager.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RuleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Singleton
+    public static RuleManager Instance;
+    
+    //moving rules from other managers here?
+    private List<int> _huntingSeasonRange;
+    private bool _huntingSeason;
+    
+
+    public bool HuntingSeason()
     {
-        
+        if (_huntingSeasonRange.Contains(TimeManager.instance.GetMonth()))
+            return true;
+        else
+        {
+            return false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool SetHuntingSeasonRange(/*Tuple<int, int> range*/)
     {
+        // _huntingSeasonRange.Add(range.Item1);
+        // int i = 1 + (range.Item2 - range.Item1);
+        // for (int a = 0; a < i; a++)
+        // {
+        //     _huntingSeasonRange.Add();
+        // }
         
     }
 }
