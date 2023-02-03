69512e6244daadea365b43c392e68064e9754a17
Aleksthor <athorset98@gmail.com>
2023-02-03 18:22:39 +0100

    Jeger Manager and navmeshagents overshootings fix

--- UNITY/MooseOrLose/Assets/Scripts/Jeger/JegerManager.cs
+++ UNITY/MooseOrLose/Assets/Scripts/Jeger/JegerManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class JegerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Jeger GameObject")]
+    public GameObject jeger;
+
+    [Header("Spawning")]
+    public int jeger_startpopulation = 5;
+    public Vector3 spawn_location;
+
+    [Header("Stats")]
+    public int jeger_population;
+
+    [Header("Array")]
+    [SerializeField] List<GameObject> jeger_list;
+
+
+    private void Start()
     {
-        
+        for (int i = 0; i < jeger_startpopulation; i++)
+        {
+            GameObject go = Instantiate(jeger, spawn_location + new Vector3(Random.Range(-10,10), 0 ,Random.Range(-10,10)), Quaternion.identity, transform);
+            jeger_list.Add(go);
+        }
+        jeger_population = jeger_startpopulation;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void AddToList(GameObject go)
     {
-        
+        jeger_population++;
+        jeger_list.Add(go);
     }
+
+    public void RemoveFromList(GameObject go)
+    {
+        jeger_population--;
+        jeger_list.Remove(go);
+    }
+
 }
