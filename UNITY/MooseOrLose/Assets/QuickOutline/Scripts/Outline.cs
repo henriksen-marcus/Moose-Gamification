e0f46a8a5ed690c636f056f0df0a38a1da02e5af
Aleksthor <athorset98@gmail.com>
2023-03-09 21:44:41 +0100

    Implemented Antlers and new Moose Meshes. Antlers mesh will have exact right number of tags

--- UNITY/MooseOrLose/Assets/QuickOutline/Scripts/Outline.cs
+++ UNITY/MooseOrLose/Assets/QuickOutline/Scripts/Outline.cs
@@ -101,14 +101,18 @@ public class Outline : MonoBehaviour {
 
   void OnEnable() {
     foreach (var renderer in renderers) {
+        if(renderer != null)
+        { 
+             // Append outline shaders
+             var materials = renderer.sharedMaterials.ToList();
 
-      // Append outline shaders
-      var materials = renderer.sharedMaterials.ToList();
+             materials.Add(outlineMaskMaterial);
+             materials.Add(outlineFillMaterial);
 
-      materials.Add(outlineMaskMaterial);
-      materials.Add(outlineFillMaterial);
+             renderer.materials = materials.ToArray();
 
-      renderer.materials = materials.ToArray();
+        }
+
     }
   }
 
@@ -139,14 +143,16 @@ public class Outline : MonoBehaviour {
 
   void OnDisable() {
     foreach (var renderer in renderers) {
+            if (renderer != null)
+            {
+                // Remove outline shaders
+                var materials = renderer.sharedMaterials.ToList();
 
-      // Remove outline shaders
-      var materials = renderer.sharedMaterials.ToList();
+                materials.Remove(outlineMaskMaterial);
+                materials.Remove(outlineFillMaterial);
 
-      materials.Remove(outlineMaskMaterial);
-      materials.Remove(outlineFillMaterial);
-
-      renderer.materials = materials.ToArray();
+                renderer.materials = materials.ToArray();
+            }
     }
   }
 
