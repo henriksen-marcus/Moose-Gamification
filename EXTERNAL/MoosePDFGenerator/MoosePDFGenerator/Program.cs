39e9023e7df83b07488586f8b19c0ae7204341db
Aleksthor <athorset98@gmail.com>
2023-04-04 16:54:38 +0200

    Squashed commit of the following:
    commit 802fbafa44735c95f204c3714987851e2b2e4e75
    Author: Marcus Henriksen <marcus.henriksen@live.no>
    Date:   Thu Mar 30 01:18:29 2023 +0200
    
        Begin work on external PDF exporter
    
        - Began work on a separate exe that will run PDFsharp.
    
    commit a77e8bba8c13971b56ba5cd9323786eb64f1725b
    Author: Marcus Henriksen <marcus.henriksen@live.no>
    Date:   Thu Mar 30 01:17:09 2023 +0200
    
        Remove DLLs
    
        PDFSharp will not work in Unity. Removing redundant files.
    
    commit 1fda17966d16ff16ba7d89fac5d4265fe58bbbdd
    Author: Marcus Henriksen <marcus.henriksen@live.no>
    Date:   Wed Mar 22 21:46:04 2023 +0100
    
        Added DLLs for PDFSharp

---
+++ EXTERNAL/MoosePDFGenerator/MoosePDFGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoosePDFGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PDFExporter.Instance.GeneratePDF(args[0]);
+        }
+    }
+}
