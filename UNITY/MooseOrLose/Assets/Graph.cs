503affa74a3756b407a552a0516c0f3681e0c304
Aleksthor <athorset98@gmail.com>
2023-04-10 21:00:26 +0200

    Update UI, Added statistics Lines

--- UNITY/MooseOrLose/Assets/Graph.cs
+++ UNITY/MooseOrLose/Assets/Graph.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Graph : MonoBehaviour
 {
     [SerializeField] private Sprite dot;
+    [SerializeField] private GameObject Line;
 
     private RectTransform container;
     private RectTransform settings;
@@ -15,14 +16,11 @@ public class Graph : MonoBehaviour
     bool mooseFemales;
     bool mooseChildren;
     bool forest1;
-
-    private int highest;
     // Start is called before the first frame update
     void Awake()
     {
         container = GetComponent<RectTransform>();
         settings = transform.parent.transform.Find("Background").transform.Find("Settings").GetComponent<RectTransform>();
-        highest = 150;
 
         moosePopulation = false;
         mooseMales = false;
@@ -81,6 +79,15 @@ public class Graph : MonoBehaviour
     {
         float height = container.sizeDelta.y - 20;
         float width = container.sizeDelta.x - 20;
+        float highest = 150;
+        for (int i = 0; i < valueList.Count; i++)
+        {
+            if (valueList[i] > highest)
+            {
+                highest = valueList[i];
+            }
+        }
+
         float yMax = highest + 50;
         float xMin = 10;
         float increment;
@@ -94,15 +101,23 @@ public class Graph : MonoBehaviour
         }
 
         for (int i = 0; i < valueList.Count; i++)
-        {
-            if (valueList[i] > highest)
-            {
-                highest = valueList[i];
-            }
+        {            
             float xPos = xMin + (i * increment);
             float yPos = (valueList[i] / yMax) * height;
             CreateCircle(new Vector2(xPos, yPos), color);
         }
+
+        for (int i = 0; i < valueList.Count - 1; i++)
+        {
+            float xPos1 = xMin + (i * increment);
+            float yPos1 = (valueList[i] / yMax) * height;
+
+            float xPos2 = xMin + ((i + 1) * increment);
+            float yPos2 = (valueList[i + 1] / yMax) * height;
+            CreateLine(new Vector2(xPos1, yPos1), new Vector2(xPos2, yPos2), color);
+        }
+
+
     }
 
     void CreateCircle(Vector2 anchoredPosition, Color color)
@@ -118,6 +133,23 @@ public class Graph : MonoBehaviour
         rectTransform.anchorMax = new Vector2(0, 0);
 
     }
+    private void CreateLine(Vector2 pos1, Vector2 pos2, Color color)
+    {
+        GameObject go = Instantiate(Line, container);
+        Image image = go.GetComponentInChildren<Image>();
+        image.color = color;
+        RectTransform rectTransform = go.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = pos1 + ((pos2-pos1) / 2f);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2((pos2 - pos1).magnitude, 2);
+        Vector3 rotation = rectTransform.transform.eulerAngles;
+        float angle = Vector2.Angle(new Vector2(1,0), pos2 - pos1);
+        rotation.z = angle;
+        rectTransform.transform.eulerAngles = rotation;
+
+    }
+
     public void updateBooleans() 
     {  
         if (settings == null || container == null)
@@ -134,4 +166,6 @@ public class Graph : MonoBehaviour
             UpdateGraphMenu();
     }
 
+
+    
 }
