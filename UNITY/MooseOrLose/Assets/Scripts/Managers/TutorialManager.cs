cd79bf1bc072404a85df92f4a0bb0d0a3f18b58a
Aleksthor <athorset98@gmail.com>
2023-04-13 16:22:03 +0200

    Fixed small bug

--- UNITY/MooseOrLose/Assets/Scripts/Managers/TutorialManager.cs
+++ UNITY/MooseOrLose/Assets/Scripts/Managers/TutorialManager.cs
@@ -141,7 +141,7 @@ public class TutorialManager : MonoBehaviour
 
     public void EndTutorial()
     {
-        tutorialItems[currentLayer].transform.SetSiblingIndex(lastChildIndex);
+        tutorialItems[currentLayer - 1].transform.SetSiblingIndex(lastChildIndex);
 
         TimeManager.instance.SetGamePaused(false);
         Destroy(gameObject);
