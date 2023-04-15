421a67857b84baa07576fa7f79951f9bf75a287b
Aleksthor <athorset98@gmail.com>
2023-04-15 02:55:41 +0200

    More Safety around pausing agents

--- UNITY/MooseOrLose/Assets/Scripts/BehaviorTree/Tasks/Elg/ElgMatingSeason.cs
+++ UNITY/MooseOrLose/Assets/Scripts/BehaviorTree/Tasks/Elg/ElgMatingSeason.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BehaviorTrees;
+using UnityEngine.AI;
 public class ElgMatingSeason : Node
 {
     Elg mScript;
@@ -11,6 +12,18 @@ public class ElgMatingSeason : Node
     }
     public override NodeState Evaluate()
     {
+        if (TimeManager.instance.gamePaused)
+        {           
+            if (mScript.GetComponent<NavMeshAgent>().isOnNavMesh)
+                mScript.GetComponent<NavMeshAgent>().isStopped = true;
+        }
+        else
+        {
+            if (mScript.GetComponent<NavMeshAgent>().isOnNavMesh)
+                mScript.GetComponent<NavMeshAgent>().isStopped = false;
+        }
+
+
         if (TimeManager.instance.MatingSeason() && mScript.gender == Gender.Female)
         {
             if (!mScript.hasMated && mScript.GetAge() > 2f && mScript.GetAge() < 17f)
