65325044cf9f80891006b500bb2c62a760fe6ae2
Aleksthor <athorset98@gmail.com>
2023-04-23 01:39:30 +0200

    Merged the UI's and rescaled all UI elements

--- UNITY/MooseOrLose/Assets/Scripts/UI/PauseMenu.cs
+++ UNITY/MooseOrLose/Assets/Scripts/UI/PauseMenu.cs
@@ -6,7 +6,8 @@ public class PauseMenu : MonoBehaviour
 {
     public static PauseMenu instance;
     [SerializeField] private GameObject Menu;
-
+    [SerializeField] private GameObject SaveGame;
+    private GameObject saveGame;
     private void Awake()
     {
         if (instance == null)  instance = this;
@@ -31,11 +32,21 @@ public class PauseMenu : MonoBehaviour
         {
             TimeManager.instance.SetGamePaused(!TimeManager.instance.gamePaused);
             ToggleMenu();
+            if (saveGame)
+            {
+                Destroy(saveGame);
+                saveGame = null;
+            }
         }
     }
 
     public void Close()
     {
         Menu.SetActive(false);
+    }
+
+    public void OpenSaveGameUI()
+    {
+        saveGame = Instantiate(SaveGame, GameObject.Find("Screen Canvas").transform);
     }
 }
